f03df9d23d285cf482c8efc51f45eff5a544cfaf
alejandro.arroyo.duque.contractor <alejandro.arroyo.duque.contractor@bbva.com>
2019-03-24 18:12:53 +0100

    fix(select): redesign

--- src/amiibo-selector.js
+++ src/amiibo-selector.js
@@ -43,17 +43,47 @@ export class AmiiboSelector extends LitElement {
         height: 1.8rem;
       }
 
+      .container {
+        padding: 0 15px 0 5px;
+        margin: 0;
+        overflow: hidden;
+        background-color: transparent;
+        background: transparent url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAPCAYAAADzun+cAAAAdUlEQVRIS73V0Q3AIAhFUdiso7hZ46QYTWraiAhUeQucv3eRiBIA3BC7jNULxjMipgYH4g2tXocD8I4O8EH8g7LwAXxAp/BGnEVFeAM+RZfwD1xEVbADX6Jq2ICrUBOswNWoGRZwE+qCGdyMuuEXfj3fa41bAUwESa2ukmCSAAAAAElFTkSuQmCC') no-repeat;
+        background-position-x: 97%;
+        background-position-y: 50%;
+        background-size: .5rem;
+        border-bottom: solid 1px white;
+      }
+    
+      .container select {
+        color: white;
+        border: none;
+        box-shadow: none;
+        background-color: transparent;
+        background-image: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+      }
+      
+      .container select:focus {
+          outline: none;
+      }
+
     `;
   } 
 
   render() {
     return html`
+    <div class="container">
       <label>${this.label}
         <select @change="${this.selectedOptionChange}">
           <option value="${this.placeholderValue}">${this.placeholder}</option>
           ${this.options.map(option => html`<option value="${option}">${option}</option>`)}
         </select>
       </label>
+    </div>
+
     `;
   }
 }
